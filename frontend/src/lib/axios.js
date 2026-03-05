0cf523a5d965e1910d7787399bf8e53f34c31d49
YDK <dineshkrishnayeturi@gmail.com>
2026-03-05 17:19:12 +0530

    added few changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,8 +3,11 @@ import axios from "axios";
 // Reads VITE_API_URL from the .env file
 // In dev:  http://localhost:5001/api/notes
 // In prod: https://your-backend.vercel.app/api/notes  (just change the .env)
+
+const BASE_URL = import.meta.end.MODE === "development" ? "http://localhost:5001" : "/api";
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: BASE_URL,
 });
 
 export default api;
