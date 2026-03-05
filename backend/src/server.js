b8e6e88187b6dbfe0e1a9eb45b950d6b9e92d605
YDK <dineshkrishnayeturi@gmail.com>
2026-03-05 17:53:18 +0530

    Resolved spelling mistake

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,16 @@ import cors from "cors";
 import notesRoutes from "./routes/notesRoute.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import * as path from "node:path";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 // middleware
 if (process.env.NODE_ENV !== "production") {
@@ -24,10 +27,12 @@ app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
     });
 }
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // In dev:  http://localhost:5001/api/notes
 // In prod: https://your-backend.vercel.app/api/notes  (just change the .env)
 
-const BASE_URL = import.meta.end.MODE === "development" ? "http://localhost:5001" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/notes" : "/api/notes";
 
 const api = axios.create({
     baseURL: BASE_URL,
